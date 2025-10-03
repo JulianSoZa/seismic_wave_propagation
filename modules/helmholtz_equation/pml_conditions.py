c4b367f5f848a8687ae46261ebd5a6319108b733
JulianSoZa <juliansoza19@gmail.com>
2025-10-03 10:10:49 -0500

    ✂️ Update pml_conditions.py
    Use functions in the example of use

--- modules/helmholtz_equation/pml_conditions.py
+++ modules/helmholtz_equation/pml_conditions.py
@@ -1,7 +1,9 @@
+#%% Libraries
 import numpy as np
 from scipy.sparse import csr_matrix
 from scipy.sparse.linalg import spsolve
 
+#%% Solver function
 def helmholtz_pml_solution(domain, frequency, velocity, source, alpha):
     """
     Solve the Helmholtz equation with PML boundary conditions.
@@ -70,7 +72,7 @@ def helmholtz_pml_solution(domain, frequency, velocity, source, alpha):
 
     tA = lambda x, y, ax, ay: ((1-1j*sigma_y(y, ay)/omega)/(1-1j*sigma_x(x, ax)/omega))
     tB = lambda x, y, ax, ay: ((1-1j*sigma_x(x, ax)/omega)/(1-1j*sigma_y(y, ay)/omega))
-    tC = lambda x, y, ax, ay: (((1-1j*sigma_y(y, ay)/omega))*((1-1j*sigma_x(x, ax)/omega))*(omega/velocity[x, y])**2)
+    tC = lambda x, y, ax, ay: ((1-1j*sigma_y(y, ay)/omega)*(1-1j*sigma_x(x, ax)/omega)*(omega/velocity[x, y])**2)
 
     num = lambda x,y: nx*y+x
 
@@ -130,7 +132,7 @@ def helmholtz_pml_solution(domain, frequency, velocity, source, alpha):
             col.append(int(num(i,j-1)))
 
         else:
-            print('no')
+            # print(f'The {k} point is not part of the domain') # for debugging
             data.append(1)
             row.append(k)
             col.append(k)
@@ -149,86 +151,39 @@ def helmholtz_pml_solution(domain, frequency, velocity, source, alpha):
         u_array_2D[i, j] = U[k]
         b_array_2D[i, j] = b[k]
 
-    print('Se soluciona el campo en cartesianas')
+    print('Solution computed')
 
     return u_array_2D, b_array_2D
 
+#%% Example of use
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
-    import matplotlib.patches as patches
     import sources
+    from domains import HelmholtzDomain
+    import plotting
 
+    # Define domain
     main_domain_shape = (201, 201)
     main_domain_extension = (-0.5, 0.5, -0.5, 0.5)
-    nbl = 100
-
-    nx = main_domain_shape[0] + nbl*2
-    ny = main_domain_shape[1] + nbl*2
-
-    lpml = (main_domain_extension[1] - main_domain_extension[0])/(main_domain_shape[0]-1) * nbl
-
-    domain_shape = (nx, ny)
-    domain_extension = tuple(x - lpml if i % 2 == 0 else x + lpml for i, x in enumerate(main_domain_extension))
-
-    x_array = np.linspace(domain_extension[0], domain_extension[1], nx)
-    y_array = np.linspace(domain_extension[2], domain_extension[3], ny)
+    domain = HelmholtzDomain(main_domain_shape, main_domain_extension)
+    domain.pml_domain(100)
     
-    dx = x_array[1] - x_array[0]
-    dy = y_array[1] - y_array[0]
-    
-    nk = nx * ny
-    points_number = np.arange(nk)
-    
+    # Define source
+    source = sources.GaussianSource(amplitude=1, x_pos=0, y_pos=0.4, sigma=0.05, phase=0)
+
+    # Define parameters
     frequency = 10
     velocity = 1.5
-    
-    velocity_array = np.ones((nx, ny))*velocity
-    velocity_array[:, :(ny-1)//2] = velocity*2
-    
+
+    velocity_array = np.ones((domain.nx, domain.ny))*velocity
+    velocity_array[:, :(domain.ny-1)//2] = velocity*2
+
     alpha = 1.4
-    
-    source = sources.GaussianSource(amplitude=1, x_pos=0, y_pos=0.4, sigma=0.05, phase=np.pi/8)
 
-    u, b = helmholtz_pml_solution(nx, ny, x_array, y_array, dx, dy, points_number, nk, frequency,
-                                                                         nbl, velocity_array, lpml, source, alpha)
+    # Solve
+    u, b = helmholtz_pml_solution(domain, frequency, velocity_array, source, alpha)
 
-    fig, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(18, 6))
-    rect_params = dict(
-        xy=(main_domain_extension[0], main_domain_extension[2]), 
-        width=main_domain_extension[1]-main_domain_extension[0], 
-        height=main_domain_extension[3]-main_domain_extension[2], 
-        linewidth=1, edgecolor='r', facecolor='none')
-    
-    vmax = np.max(np.abs(b))
-    im0 = ax0.imshow(np.real(b).T, extent=domain_extension, origin='lower', cmap='PRGn', vmin=-vmax, vmax=vmax)
-    plt.colorbar(im0, ax=ax0, shrink=0.5)
-    ax0.add_patch(patches.Rectangle(**rect_params))
-    ax0.set_title('Source')
-    ax0.set_xlabel('x')
-    ax0.set_ylabel('y')
-
-    vmax = np.max(np.abs(np.real(u)))
-    im1 = ax1.imshow(np.real(u).T, extent=domain_extension, origin='lower', cmap='seismic', vmin=-vmax, vmax=vmax)
-    plt.colorbar(im1, ax=ax1, shrink=0.5)
-    ax1.add_patch(patches.Rectangle(**rect_params))
-    ax1.set_title('Field')
-    ax1.set_xlabel('x')
-    ax1.set_ylabel('y')
-
-    im2 = ax2.imshow(np.angle(u).T, extent=domain_extension, origin='lower', cmap='twilight', vmin=-np.pi, vmax=np.pi)
-    plt.colorbar(im2, ax=ax2, shrink=0.5)
-    ax2.add_patch(patches.Rectangle(**rect_params))
-    ax2.set_title('Phase')
-    ax2.set_xlabel('x')
-    ax2.set_ylabel('y')
-
-    fig.tight_layout()
-    
-    fig2 = plt.figure()
-    plt.imshow(velocity_array.T, extent=domain_extension, origin='lower', cmap='viridis')
-    plt.colorbar(shrink=0.5)
-    plt.title('Velocity')
-    plt.xlabel('x')
-    plt.ylabel('y')
-
+#%% Plotting
+    plotting.plot_solution(domain, np.array([frequency]), 0, u[..., np.newaxis], b[..., np.newaxis])
+    plotting.plot_velocity(domain, velocity_array)
     plt.show()
